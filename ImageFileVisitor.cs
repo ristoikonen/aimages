5e4348af9bbcb2e5d91858e7aed78923c157c9bf
Risto Ikonen <ristoikonen@gmail.com>
2025-01-28 19:28:24 +1100

    Serves image data with desc read. End to end test works.

---
+++ ImageFileVisitor.cs
@@ -0,0 +1,51 @@
+﻿using aimages.Models;
+using System.IO;
+
+namespace aimages
+{
+    public class ImageFileVisitor
+    {
+        const string TXT_EXT= ".txt";
+        private string PathToFiles { get; set; }
+        private string PathToSubDirImageTextFiles { get; set; }
+
+        public ImageFileVisitor(string pathToImageFiles, string pathToSubDirImageTextFiles)
+        {
+            PathToFiles= pathToImageFiles;
+            PathToSubDirImageTextFiles = pathToSubDirImageTextFiles;
+        }
+
+        // Create AImage object by reading names, filepaths and descrption file (sits in a subdir)
+        public List<AImage> ReadDirectory(string pathToFiles) 
+        {
+            List<AImage> list = new List<AImage>();
+            string desc = "";
+
+            foreach (var filename in Directory.GetFiles(pathToFiles))
+            {
+                string subdirForTexFiles = Directory.GetDirectories(pathToFiles, PathToSubDirImageTextFiles).FirstOrDefault<string>() ?? "";
+                // any filetype, root
+                //var filenames = Directory.GetFiles(pathToFiles, "*", SearchOption.AllDirectories).Select(x => Path.GetFileName(x)).ToArray();
+
+                //foreach (var textfilename in filenames)
+                //{
+                // any filetype, subdir
+                //var textfile = System.IO.Directory.GetFiles(subdirForTexFiles, textfilename + ".*", System.IO.SearchOption.AllDirectories).FirstOrDefault<string>() ?? "";
+
+                var textfile = Path.Combine(subdirForTexFiles, Path.GetFileNameWithoutExtension(filename) + TXT_EXT );
+
+                //var textfile = System.IO.Directory.GetFiles(subdirForTexFiles, textfilename + ".*", System.IO.SearchOption.AllDirectories).FirstOrDefault<string>() ?? "";
+
+                if (File.Exists(textfile))
+                    {
+                        // Console.WriteLine("Reading:" + textfile);
+                        desc = File.ReadAllText(textfile) ?? "";
+                    }
+                //}
+                list.Add(new AImage(Path.GetFileName(filename) ?? "", filename, desc));
+            }
+            return list;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
+using aimages;
 using aimages.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Buffers;
+using System.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost",
+                                "http://localhost:4200");
+        });
+});
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+//app.UseCors(x => x
+//            .AllowAnyOrigin()
+//            .AllowAnyMethod()
+//            .AllowAnyHeader());
+
+
+
 
 
 // Configure the HTTP request pipeline.
@@ -16,10 +38,16 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors();
+
+
 
 //=---------------------------------
 // /api/v{version:apiVersion}
 // .WithApiVersionSet(versionSet)
+
+
+
 var pathToImages = @"C:\tmp\images";
 
 
@@ -28,8 +56,8 @@ var imagesGroup = app
     .WithTags("Images");
 
 
-imagesGroup.MapGet(string.Empty, GetAImages)
-    .WithName("GetImages")
+imagesGroup.MapGet(string.Empty, GetAImageDetails)
+    .WithName("GetImageDetails")
     .WithOpenApi(operation =>
      {
         operation.Summary = "Get a list of textual image data";
@@ -37,6 +65,51 @@ imagesGroup.MapGet(string.Empty, GetAImages)
         return operation;
     });
 
+imagesGroup.MapGet("names", GetAImages)
+    .WithName("GetImages")
+    .WithOpenApi(operation =>
+    {
+        operation.Summary = "Get a list names";
+        operation.Description = "List names";
+        return operation;
+    });
+
+// (int? pageNumber) => $"Requesting page {pageNumber ?? 1}
+
+
+imagesGroup.MapGet("search/{desc}", SearchByDesc)
+    .WithName("SearchByDesc")
+    .WithOpenApi(operation =>
+    {
+        operation.Summary = "Search for image(s)";
+        operation.Description = "Search of text in images Desc -field";
+        return operation;
+    });
+
+imagesGroup.MapGet("id/{desc}", (  [FromQuery(Name = "desc")] string desc )
+                     => { return "ABC"; });
+
+
+//imagesGroup.MapGet("search/{desc}", ([FromQuery(Name = "desc")] string desc)
+//                     => { return "ABC"; });
+
+
+//imagesGroup.MapGet("images/{desc}", SearchByDesc)
+//    .WithName("SearchByDesc")
+//    .WithOpenApi(operation =>
+//    {
+//        operation.Summary = "Search for image(s)";
+//        operation.Description = "Search of text in images Desc -field";
+//        return operation;
+//    });
+//imagesGroup.MapGet(string.Empty, SearchByDesc)
+//    .WithName("SearchByDesc")
+//    .WithOpenApi(operation =>
+//    {
+//        operation.Summary = "Search for image(s)";
+//        operation.Description = "Search of text in images Desc -field";
+//        return operation;
+//    });
 
 
 /*
@@ -49,10 +122,47 @@ app.MapGet("/images", () =>
 */
 
 
-List<AImage> GetAImages()
+List<AImage> GetAImageDetails()
 {
     return new List<AImage>(aimages.DirAnalyser.GetImages(pathToImages));
 
+}
+
+IResult GetAImages()
+{
+    return Results.Json("Test me"); // List<AImage>(aimages.DirAnalyser.GetImages(pathToImages));
+}
+
+
+List<AImage> SearchByDesc(string desc)
+{
+    List<AImage> lista = new List<AImage>();
+
+    // get collection of AImages
+    ImageFileVisitor visitor = new ImageFileVisitor(pathToImages, "Descriptions");
+    var list = visitor.ReadDirectory(pathToImages);
+
+    foreach ( AImage image in list )
+    { 
+
+        // search for text in 'Desc' field of AImages
+        if (!string.IsNullOrWhiteSpace(image.Desc))
+        {
+            SearchValues<string> searchValues = SearchValues.Create(image.Desc.Split(new char[] { ' ', '.', ',', ';', ':', '!', '?' }, StringSplitOptions.RemoveEmptyEntries), StringComparison.OrdinalIgnoreCase);
+            //var ixx = image.Desc.AsSpan().ContainsAny(desc);
+
+            // testing BinarySearch..
+            var splitArray = image.Desc.Split(new char[] { ' ', '.', ',', ';', ':', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            int index = Array.BinarySearch(splitArray, desc);
+
+            if (searchValues.Contains(desc))
+            {
+                lista.Add(image);
+            }
+        }
+
+    }
+    return lista;
 }
 
 
